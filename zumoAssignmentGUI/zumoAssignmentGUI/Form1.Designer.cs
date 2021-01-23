6bc36d6e26fae0dd7b15821318d87111b4023bbf
Tom <tomcastineiras@gmail.com>
2021-01-23 10:30:28 +0000

    Basic GUI Design

--- zumoAssignmentGUI/zumoAssignmentGUI/Form1.Designer.cs
+++ zumoAssignmentGUI/zumoAssignmentGUI/Form1.Designer.cs
@@ -29,13 +29,68 @@ namespace zumoAssignmentGUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.ForwardButton = new System.Windows.Forms.Button();
+            this.TurnRightButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.TurnLeftButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ForwardButton
+            // 
+            this.ForwardButton.Location = new System.Drawing.Point(168, 12);
+            this.ForwardButton.Name = "ForwardButton";
+            this.ForwardButton.Size = new System.Drawing.Size(150, 75);
+            this.ForwardButton.TabIndex = 0;
+            this.ForwardButton.Text = "Forward";
+            this.ForwardButton.UseVisualStyleBackColor = true;
+            // 
+            // TurnRightButton
+            // 
+            this.TurnRightButton.Location = new System.Drawing.Point(324, 93);
+            this.TurnRightButton.Name = "TurnRightButton";
+            this.TurnRightButton.Size = new System.Drawing.Size(150, 75);
+            this.TurnRightButton.TabIndex = 1;
+            this.TurnRightButton.Text = "Turn Right";
+            this.TurnRightButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(168, 174);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(150, 75);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // TurnLeftButton
+            // 
+            this.TurnLeftButton.Location = new System.Drawing.Point(12, 93);
+            this.TurnLeftButton.Name = "TurnLeftButton";
+            this.TurnLeftButton.Size = new System.Drawing.Size(150, 75);
+            this.TurnLeftButton.TabIndex = 3;
+            this.TurnLeftButton.Text = "Turn Left";
+            this.TurnLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(168, 93);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(150, 75);
+            this.StopButton.TabIndex = 4;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.TurnLeftButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.TurnRightButton);
+            this.Controls.Add(this.ForwardButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +98,12 @@ namespace zumoAssignmentGUI
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ForwardButton;
+        private System.Windows.Forms.Button TurnRightButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button TurnLeftButton;
+        private System.Windows.Forms.Button StopButton;
     }
 }
 
